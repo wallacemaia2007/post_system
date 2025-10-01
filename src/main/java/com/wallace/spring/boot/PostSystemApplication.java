c3b16e9d9a4cb0e60fb1234513b09bc453f21956
Wallace Maia <wallacemaia2007@gmail.com>
2025-10-01 16:12:47 -0300

    Create Project

---
+++ src/main/java/com/wallace/spring/boot/PostSystemApplication.java
@@ -0,0 +1,13 @@
+package com.wallace.spring.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wallace/spring/boot/PostSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wallace.spring.boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
